c056e824b341c002b13e36183ac07d13caf6623a
Evan <hayesevan@rocketmail.com>
2019-04-21 12:13:46 -0400

    Initial commit for portfolio website

---
+++ src/main/java/com/ehayes/portfolio/Ehayes/Portfolio/EhayesPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.ehayes.portfolio.Ehayes.Portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EhayesPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EhayesPortfolioApplication.class, args);
+	}
+
+}
